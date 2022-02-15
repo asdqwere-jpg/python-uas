eec95d1cf31f1007082dac367ed809a6cb690d81
Melvin-Jovano <melvinjovano2@gmail.com>
2022-02-15 20:53:37 +0700

    First Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+def getTodayDate():
+    from datetime import date
+    return date.today().strftime("%d/%m/%Y")
+
+def showMenu():
+    print('Please Choose An Option :')
+    print('1. Insert New Order')  
+    print('2. Data Report')
+    print('0. Exit')
+
+def showReportMenu():
+    print('Please Choose An Option :')
+    print('1. By Today')
+    print('0. Exit')
+    
+def showMenuOrder():
+    import ast
+    paging = 0
+    menuDatabase = open('menu.json', 'r')
+    menuData = ast.literal_eval(menuDatabase.read())
+    menuDatabase.close()
+    for menu in menuData:
+        print(paging + ' ' + menu)
+        paging += 1
