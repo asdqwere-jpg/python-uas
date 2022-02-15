f1e7efb759f0ba7afd00d071bb636a2b9ee427e5
Melvin-Jovano <melvinjovano2@gmail.com>
2022-02-15 21:33:19 +0700

    2nd Commit

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+for index, item in enumerate(my_list):
+    if item.id == 'specific_id':
+        break
+    else:
+        index = -1
+
 def getTodayDate():
     from datetime import date
     return date.today().strftime("%d/%m/%Y")
